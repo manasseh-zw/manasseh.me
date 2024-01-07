28a0329e12c2b2e3c81675a3d557c113566700e8
manasseh-zw <manassehtchanga@gmail.com>
2024-01-08 00:32:07 +0200

    nojackll

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
     paths: {
       base:
         process.env.NODE_ENV === "production"
-          ? "/manasseh.me-github-pages"
+          ? "/manasseh.me"
           : "",
     },
   },
