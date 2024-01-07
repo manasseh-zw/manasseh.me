816090c8e8f4a16eb632c1a9e223dcd3a3fe1e22
manasseh-zw <manassehtchanga@gmail.com>
2024-01-07 23:10:22 +0200

    init

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base:
+        process.env.NODE_ENV === "production"
+          ? "/manasseh.me-github-pages"
+          : "",
+    },
+  },
+};
+
+export default config;
