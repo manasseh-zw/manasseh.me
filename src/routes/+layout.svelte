1dd0d4a83e523abce8aec2417efeb5810fa475c3
manasseh-zw <manassehtchanga@gmail.com>
2024-01-08 00:24:57 +0200

    home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Footer from "./components/footer.svelte";
+  import Navbar from "./components/navbar.svelte";
+</script>
+
+<Navbar />
+<slot />
+<Footer></Footer>
+
+<style>
+  @import "../css/styles.css";
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,72 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>Manasseh.me</title>
+
+<main class="main">
+  <section class="home section" id="home">
+    <div class="home__container container grid section__border">
+      <div class="home__data grid">
+        <h1 class="home__title">
+          Hi, im Manasseh <br />
+          A Full-Stack Developer <br />
+          Based in Zimbabwe
+        </h1>
+
+        <div class="home__blob grid">
+          <div class="home__perfil">
+            <img src="/img/profile-me-2.jpg" />
+          </div>
+        </div>
+
+        <ul class="home__social">
+          <a href="https://www.linkedin.com/" class="home__social-link">
+            <i class="bx bxl-linkedin"></i>
+          </a>
+          <a href="https://github.com/manasseh-zw/" class="home__social-link">
+            <i class="bx bxl-github"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/manasseh.zw/"
+            class="home__social-link"
+          >
+            <i class="bx bxl-instagram"></i>
+          </a>
+          <a href="https://dribbble.com/Manaboy729/" class="home__social-link">
+            <i class="bx bxl-dribbble"></i>
+          </a>
+        </ul>
+      </div>
+
+      <!-- home info -->
+
+      <div class="home__info">
+        <div>
+          <h2 class="home__info-title">Bio</h2>
+          <p class="home__info-description">
+            I'm Manasseh, a Full-Stack Developer. <br />
+            I love what i do, <br />
+            Delivering elegant digital solutions.
+          </p>
+        </div>
+
+        <div>
+          <h2 class="home__info-title">Contact</h2>
+          <p class="home__info-description">
+            Harare, Zimbabwe <br />
+            dev.manasseh@gmail.com <br />
+            +263 777724726
+          </p>
+        </div>
+
+        <div>
+          <h2 class="home__info-title">Services</h2>
+          <p class="home__info-description">
+            Web Design <br />
+            UI/UX Design<br />
+
+            Web APIs <br />
+            Dev-Ops <br />
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
